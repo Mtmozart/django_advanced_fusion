e9ef1b1a23d8601829a081e5b19ba2b54ca5b344
TheSamuraiBr <mmsnborges@gmail.com>
2023-05-26 12:23:13 -0300

    redirect logout

--- fusion/settings.py
+++ fusion/settings.py
@@ -143,3 +143,5 @@ DEFAULT_FROM_EMAIL = 'contato@fusion.com.br'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = 'index'
