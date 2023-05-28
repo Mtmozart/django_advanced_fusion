5c50fadfdeeedfcc04af45fe35103646380af8ac
TheSamuraiBr <mmsnborges@gmail.com>
2023-05-28 13:02:14 -0300

    correction

--- fusion/settings.py
+++ fusion/settings.py
@@ -39,8 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'core',
+    #apagar caso não seja necessário
+    'django_adminlte',
+    'django_adminlte_theme',
 ]
 
 MIDDLEWARE = [
@@ -127,11 +129,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = BASE_DIR /'media'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 #E-mail test console
 
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
