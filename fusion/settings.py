af016d799312fcab850a8a4458a64da7bc4a0dc9
TheSamuraiBr <mmsnborges@gmail.com>
2023-05-29 11:14:09 -0300

    corrections

--- fusion/settings.py
+++ fusion/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
-STATIC_ROOT = os.path(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
