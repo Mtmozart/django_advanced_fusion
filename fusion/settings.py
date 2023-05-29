47c74a36c82871f2baedc0f8ba9614bddf92145d
TheSamuraiBr <mmsnborges@gmail.com>
2023-05-29 11:20:30 -0300

    corrections

--- fusion/settings.py
+++ fusion/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i+ek6dkw*01sve=7$v39pbbd1&o1evpe9+f3c%6ajmt(hv@pna'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'fusion.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -88,10 +88,11 @@ DATABASES = {
     }
 }
 """
-
+Quando for fazer o deploy
 DATABASES = {
     'default': dj_database_url.config()
 }
+"""
 
 
 
